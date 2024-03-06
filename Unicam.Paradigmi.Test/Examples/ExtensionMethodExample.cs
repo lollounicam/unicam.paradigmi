8d24fa430fe8f736e2f3a53879f99e6efaf7583a
LORENZO POMPILI <lorenzo.pompili@studenti.unicam.it>
2024-03-06 19:09:34 +0100

    Extension Method

---
+++ Unicam.Paradigmi.Test/Examples/ExtensionMethodExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unicam.Paradigmi.Abstractions;
+using Unicam.Paradigmi.Test.Extension;
+
+namespace Unicam.Paradigmi.Test.Examples
+{
+    public class ExtensionMethodExample : IExample
+    {
+        public void RunExample()
+        {
+            string cognome = "Pompili";
+            bool isValid = cognome.PiuCortaDi(40);
+        }
+    }
+}
