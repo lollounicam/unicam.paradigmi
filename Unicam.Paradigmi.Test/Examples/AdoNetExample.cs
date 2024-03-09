703c1ad32268c4ea03da7e277e8c285e28db5352
LORENZO POMPILI <lorenzo.pompili@studenti.unicam.it>
2024-03-09 17:05:34 +0100

    Connessione db, lettura e scrittura dati.

--- Unicam.Paradigmi.Test/Examples/AdoNetExample.cs
+++ Unicam.Paradigmi.Test/Examples/AdoNetExample.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unicam.Paradigmi.Abstractions;
+using Unicam.Paradigmi.Test.Models;
 
 namespace Unicam.Paradigmi.Test.Examples
 {
@@ -12,10 +13,53 @@ namespace Unicam.Paradigmi.Test.Examples
     {
         public void RunExample()
         {
-            var connection = new SqlConnection();
-            connection.ConnectionString = "Server=localhost;Database=Paradigmi;Trusted_Connection=True;";
-            connection.Open();
-            connection.Close();
+            GetAziende();
+        }
+
+        private List<Azienda> GetAziende()
+        {
+            List<Azienda> list = new List<Azienda>();
+            using (var connection = new SqlConnection())
+            {
+                connection.ConnectionString = "Server=localhost;Database=Paradigmi;Trusted_Connection=True;";
+                connection.Open();
+
+                var cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "SELECT IdAzienda,RagioneSociale,Cap,Citta FROM Aziende";
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var azienda = new Azienda();
+                        azienda.IdAzienda = (int)reader["IdAzienda"];
+                        azienda.RagioneSociale = (string)reader["RagioneSociale"];
+                        azienda.Cap = (string)reader["Cap"];
+                        azienda.Citta = (string)reader["Citta"];
+                        list.Add(azienda);
+                    }
+                }
+            }
+            return list;
+        }
+
+        private void AddAzienda()
+        {
+
+            using (var connection = new SqlConnection())
+            {
+                connection.ConnectionString = "Server=localhost;Database=Paradigmi;Trusted_Connection=True;";
+                connection.Open();
+
+                var cmd = new SqlCommand();
+                cmd.Connection = connection;
+                cmd.CommandText = "INSERT INTO Aziende(RagioneSociale,Citta,Cap) VALUES(@RAGIONE_SOCIALE,@CITTA,@CAP)";
+                cmd.Parameters.AddWithValue("@RAGIONE_SOCIALE", "UNICAM");
+                cmd.Parameters.AddWithValue("@CITTA", "Camerino");
+                cmd.Parameters.AddWithValue("@CAP", "98765");
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
